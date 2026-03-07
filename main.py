8b5047196d57e0f22c31dff2e9818a35cdc7b05c
Najam <realnajmiter@gmail.com>
2026-03-08 02:56:44 +0500

    intro & basic types

---
+++ main.py
@@ -0,0 +1,38 @@
+# my_name = 'CodeMite'
+
+# 1. they must start with an alphabet or an underscore
+# 2. they cannot contain any special character other than the underscore
+# 3. they must not contain an empty space
+# 4. variables names in the python are case sensitive
+
+# _m1y3N2a_m_e_ = 'asdf'
+
+# aa = 0
+# aA = 1
+# AA = 1
+# Aa = 1
+# Aa = 1
+
+# i = 120
+# f = 1.12
+
+# 1. string
+string = "this is a string"
+print(f'{string=}')
+
+# 2. number (int - integer, float)
+age = 90
+print(f'{age=}')
+
+# 3. boolean (True, False)
+is_human = True
+print(f'{is_human=}')
+
+# 4. None
+kids = None
+print(f'{kids=}')
+
+# 5. list
+is_human = False
+videos = [1, 'i am string', is_human, 'i am also a string', None]
+print(f'{videos=}')
