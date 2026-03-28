1cd5ea07d674fdfe6d8b7a9333aa638685080d55
Najam <realnajmiter@gmail.com>
2026-03-29 01:36:45 +0500

    lists

--- main.py
+++ main.py
@@ -1,74 +1,120 @@
-# age = 90
+# ages = [18, 19, 20, 18, 21]
+#       0   1   2   3   4
 
-# details = f"my age is: {age + 1}"
-# print(details)
-
-# date = 'a good day'
-
-# msg = f'{date=}'
-
-# print(msg)
-
-# pi = 3.1415926
-
-# print(f'{pi=:.1f}')
-
-# content = 'this is SoMe content'
-
-# print(content.capitalize())
-# print(content.count('this'))
-# print(content.endswith('t .'))
-# print(content.startswith('This'))
-# print(content.find('is'))
-# print(content.index('is'))
-# print(content.isalnum())
-# print(content.isalpha())
-# print(content.isascii())
-# print(content.isnumeric())
-# print(content.isspace())
-# print(content.isprintable())
-# content = content.title()
-# print(content.istitle())
-# content = content.lower()
-# print(content.islower())
-# content = content.upper()
-# print(content)
-# print(content.isupper())
-
-# string = 'we have some string value'
-
-# print(string.split('.'))
-
-# permission = 'create:user'
-
-# permissions = permission.split(':') # ['create', 'user']
-
-# action = permissions[0] # 'create'
-# resource = permissions[1] # 'user'
-
-# print(f'You have access to {action} a {resource}')
-
-# user = 'My name is terrorist and i am not a khan'
-
-# user = user.replace('terrorist and', 'Khan')
-
-# user = user.removeprefix('my')
-
-# print(user)
-
-# name = 'codemite'
-
-# print(len(name))
-# print(name[0])
-
-# NOT ALLOWED
-# name[0] = 'ysjdfkjasdf'
+# count = len(ages) # 5
+# avg_age = 0
 
 # i = 0
-# while i < len(name):
-#     print(name[i])
+# while i < count: # 5 < 5
+#     this_user_age = ages[i]
+#     avg_age += this_user_age
+    
 #     i += 1
 
-string = 'this is yet another string'
+# for this_user_age in ages:
+#     print('inside the for loop:', this_user_age)
+#     avg_age += this_user_age
 
-print(f'{string[:]}|')
+# avg_age /= count
+
+# print(f'{avg_age=}')
+
+# numbers = []
+
+# print('before appending:', numbers)
+
+# numbers.append(6)
+# numbers.append(5)
+# numbers.append(6)
+
+# print('after appending:', numbers)
+
+# numbers.clear()
+
+# numbers = [1,2,3,4] # 0 .. 20 -> 4..7 --- 12..15
+
+# copy = numbers.copy()
+# copy[0] = 67
+# print('copy:', copy)
+
+# numbers = [1,2,3,4,4,1,4]
+# count = numbers.count(4)
+
+# numbers = [1,2,3]
+# ages = [18, 19, 20, 18, 21]
+
+# numbers.extend(ages) # better
+
+# print(numbers + ages) # good
+
+
+# numbers = [10, 8, 30]
+
+# index = numbers.index(8)
+
+# numbers = [10, 8, 30] 
+
+# numbers.insert(10, 67) # 10, 67, 8, 30
+
+# numbers = [10, 8, 30] 
+
+# removed = numbers.pop()
+
+# print(f'{removed=}')
+
+# numbers = [10, 8, 30] 
+# numbers.remove(8)
+
+# numbers = [10, 8, 30, 5] 
+
+# numbers.reverse()
+# numbers.sort()
+
+# print(f'{numbers=}')
+
+# numbers = [1,200,12,4,5]
+
+# slice = numbers[:] # numbers.copy()
+# slice = numbers[0::2]
+# slice = numbers[3:]
+# slice = numbers[-3]
+# slice[0] = 123
+
+# numbers = [1,200,12,4,5]
+# slice = numbers[:1:-2]
+
+# print(f'{slice=}')
+# print(f'{numbers=}')
+
+# string = 'some textual value'
+# strings = list(string) # []
+
+# strings = [char.upper() for char in string]
+
+# strings = []
+# for char in string:
+#     strings.append(char.upper())
+
+# print(f'{strings=}')
+
+# numbers = [1,2,3,4,5]
+
+# for i, number in enumerate(numbers):
+#     print(i, number)
+
+# numbers = [1,2,3,4,5]
+
+# first = numbers[0]
+# rest = numbers[1:]
+
+# first, second, *rest, last = numbers
+# print(f'{first=}')
+# print(f'{second=}')
+# print(f'{rest=}')
+# print(f'{last=}')
+
+numbers = [1]
+first, *rest = numbers
+
+print(f'{first=}')
+print(f'{rest=}')
